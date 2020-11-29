3ce978277e60f9950751c55ce40dc8962a44eec0
JoJukov <ivanzhukov2002@mail.ru>
2020-11-29 20:45:23 +0300

    plottim)

--- lab5/monitor1_fon.bash
+++ lab5/monitor1_fon.bash
@@ -1,14 +1,21 @@
 #!/bin/bash
+>.inf_for_plot1_mem.log
+>.inf_for_plot1_swap.log
+>config.log
+i=0
 for((;;))
 do
-	sleep 5s
-	grep_c=$(top -b -n 1 | grep -E "mem[2]*.bash")
+	let i=i+1
+	sleep 1s
+	grep_c=$(top -b -n 1 | grep -E " mem[2]*.bash")
 	if [ -n "${grep_c}" ]
 	then
 		echo "___Info in $(date)" >> config.log
 		echo "	System mem params" >> config.log
 		echo "$(top -b -n 1 | grep -E "MiB Mem")" >> config.log
+		echo "$i $(top -b -n 1 | grep -E "MiB Mem" | grep -Eo "[0-9]+" | awk '{print $1}' | head -n 3 | tail -n 1)" >> .inf_for_plot1_mem.log
 		echo "$(top -b -n 1 | grep -E "MiB Swap")" >> config.log
+		echo "$i $(top -b -n 1 | grep -E "MiB Swap" | grep -Eo "[0-9]+" | awk '{print $1}' | head -n 3 | tail -n 1)" >> .inf_for_plot1_swap.log
 		echo >> config.log
 		echo "	Info about mem.bash" >> config.log
 		echo "${grep_c}" >> config.log
@@ -17,6 +24,7 @@ do
 		echo "$(top -b -n 1 | head -n 12 | tail -n 5)" >> config.log
 		echo >> config.log
 	else
-		killall mem_together.bash 2>/dev/null
+		./monitor1.bash
+		exit
 	fi
 done
--- lab5/monitor2_fon.bash
+++ lab5/monitor2_fon.bash
@@ -1,14 +1,21 @@
 #!/bin/bash
+>.inf_for_plot2_mem.log
+>.inf_for_plot2_swap.log
+>config2.log
+i=0
 for((;;))
 do
-	sleep 5s
+	sleep 1s
+	let i=i+1
 	grep_c=$(top -b -n 1 | grep -E "mem[2]*.bash")
 	if [ -n "$grep_c" ]
 	then
 		echo "___Info in $(date)" >> config2.log
 		echo "	System mem params" >> config2.log
 		echo "$(top -b -n 1 | grep -E "MiB Mem")" >> config2.log
+		echo "$i $(top -b -n 1 | grep -E "MiB Mem" | grep -Eo "[0-9]+" | awk '{print $1}' | head -n 3 | tail -n 1)" >> .inf_for_plot2_mem.log
 		echo "$(top -b -n 1 | grep -E "MiB Swap")" >> config2.log
+		echo "$i $(top -b -n 1 | grep -E "MiB Swap" | grep -Eo "[0-9]+" | awk '{print $1}' | head -n 3 | tail -n 1)" >> .inf_for_plot2_swap.log
 		echo >> config2.log
 		echo "	Info about mem_together.bash" >> config2.log
 		echo "${grep_c}" >> config2.log
